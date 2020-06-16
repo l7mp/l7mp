ac741117a0584e514970c4cd1fc01fd1e5b88b3a
Gabor Retvari <retvari@tmit.bme.hu>
2020-06-16 17:34:01 +0200

    Further debug msg

--- cluster.js
+++ cluster.js
@@ -384,7 +384,7 @@ class JSONSocketEndPoint extends EndPoint {
     constructor(c, e) {
         super(c, e);
         e.name = e.name + '-transport-endpoint';
-        if(!this.cluster.transport)console.trace(dumper(this, 5));
+        if(!this.cluster.transport){console.trace(dumper(this.cluster, 5));console.trace(dumper(this.cluster.transport, 5))}
         this.cluster.transport.addEndPoint(e);
     }
 
