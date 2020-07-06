36d66a12c5ad4049bd12d4f282b3f0fc72937bbe
davidkornel <konel1020@gmail.com>
2020-07-06 10:31:22 +0200

    Finished uds listener case

--- test/12-uds-listener.test.js
+++ test/12-uds-listener.test.js
@@ -6,7 +6,7 @@ const L7mp         = require('../l7mp.js').L7mp;
 const Listener     = require('../listener.js').Listener;
 const Session      = require('../session.js').Session;
 
-describe('TCPListener', ()  => {
+describe('UDSListener', ()  => {
     var l, s, c;
     var readers = {};
     var reader = (name, stream, done) => {
@@ -29,15 +29,14 @@ describe('TCPListener', ()  => {
     });
 
     context('create', () => {
-        it('created',      () => { l = Listener.create( {name: 'TCP', spec: { protocol: 'TCP', port: 54321 }}); assert.exists(l); });
+        it('created',      () => { l = Listener.create( {name: 'UnixDomainSocket', spec: { protocol: 'UnixDomainSocket', filename: '/tmp/unixSocket.sock' }}); assert.exists(l); });
         it('object',       () => { assert.isObject(l); });
-        // TCPListener is not exported so we cannot check from here
         it('instanceOf',   () => { assert.instanceOf(l, Listener); });
         it('Emitter',      () => { assert.instanceOf(l, EventEmitter); });
         it('has-name',     () => { assert.property(l, 'name'); });
         it('has-spec',     () => { assert.property(l, 'spec'); });
-        it('has-protocol', () => { assert.nestedPropertyVal(l, 'spec.protocol', 'TCP'); });
-        it('has-port',     () => { assert.nestedPropertyVal(l, 'spec.port', 54321); });
+        it('has-protocol', () => { assert.nestedPropertyVal(l, 'spec.protocol', 'UnixDomainSocket'); });
+        it('has-port',     () => { assert.nestedPropertyVal(l, 'spec.filename', '/tmp/unixSocket.sock'); });
         it('can-listen',   () => { l.emitter=(x) =>{ s = x }; assert.isOk(true); });
     });
 
@@ -46,10 +45,7 @@ describe('TCPListener', ()  => {
     });
 
     context('#connect', () => {
-        it('connect',    (done) => { c = new Net.connect({port: 54321}, () => { assert.isOk(true); done(); }) });
-        it('address',        () => { assert.equal(c.localAddress, '127.0.0.1'); });
-        it('remote-address', () => { assert.equal(c.remoteAddress, '127.0.0.1'); });
-        it('remote-port',    () => { assert.equal(c.remotePort, 54321); });
+        it('connect',    (done) => { c = new Net.connect({path: '/tmp/unixSocket.sock'}, () => { assert.isOk(true); done(); }) });
         it('isa stream',     () => { assert.instanceOf(c, Stream); });
         it('readable',       () => { assert.isOk(c.readable); });
         it('writeable',      () => { assert.isOk(c.writable); });
@@ -59,17 +55,11 @@ describe('TCPListener', ()  => {
         it('emits',                     () => { assert.isOk(s); });
         it('session-metadata',          () => { assert.property(s, 'metadata'); });
         it('session-metadata-name',     () => { assert.nestedProperty(s, 'metadata.name'); });
-        it('session-metadata-IP',       () => { assert.nestedProperty(s, 'metadata.IP'); });
-        it('session-metadata-src-addr', () => { assert.nestedProperty(s, 'metadata.IP.src_addr'); });
-        it('session-metadata-src-addr', () => { assert.match(s.metadata.IP.src_addr, /127.0.0.1/); });
-        it('session-metadata-dst-addr', () => { assert.nestedProperty(s, 'metadata.IP.dst_addr'); });
-        it('session-metadata-dst-addr', () => { assert.match(s.metadata.IP.dst_addr, /127.0.0.1/); });
-        it('session-metadata-TCP',      () => { assert.nestedProperty(s, 'metadata.TCP'); });
-        it('session-metadata-src-port', () => { assert.nestedProperty(s, 'metadata.TCP.src_port'); });
-        it('session-metadata-dst-port', () => { assert.nestedPropertyVal(s, 'metadata.TCP.dst_port', 54321); });
-        it('session-listener',          () => { assert.nestedPropertyVal(s, 'source.origin', 'TCP'); });
+        it('session-metadata-UNIX',      () => { assert.nestedProperty(s, 'metadata.UNIX'); });
+        it('session-metadata-filename', () => { console.log(s.metadata.UNIX.filename);assert.nestedProperty(s, 'metadata.UNIX.filename'); });
+        it('session-listener',          () => { assert.nestedPropertyVal(s, 'source.origin', 'UnixDomainSocket'); });
         it('session-stream',            () => { assert.nestedProperty(s, 'source.stream'); });
-        it('session-stream',            () => { assert.nestedProperty(s, 'source.origin', 'TCP'); });
+        it('session-stream',            () => { assert.nestedProperty(s, 'source.origin', 'UnixDomainSocket'); });
         it('session-stream',            () => { assert.instanceOf(s.source.stream, Stream) });
         it('session-stream-readable',   () => { assert.isOk(s.source.stream.readable); });
         it('session-stream-writeable',  () => { assert.isOk(s.source.stream.writable); });
