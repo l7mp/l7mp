a0ba1c63ab9be13c8adec92baf703e860f9675b0
Váradi Richárd <ricsi19981007@gmail.com>
2020-07-07 08:50:27 +0200

    New test: JSONSocket enpoint

---
+++ test/10-JSONSocket-endpoint.test.js
@@ -0,0 +1,30 @@
+const assert   = require('chai').assert;
+const L7mp     = require('../l7mp.js').L7mp;
+const EndPoint = require('../cluster.js').EndPoint;
+const Cluster  = require('../cluster.js').Cluster;
+
+describe('JSONSocket', () => {
+    var e, c, s_ok;
+    before( () => {
+        l7mp = new L7mp();
+        l7mp.applyAdmin({ log_level: 'error' });
+        l7mp.run();
+    });
+
+    context('create', () => {
+        it('runs',         () => {
+            c = Cluster.create({
+                name: 'JSONSocket',
+                spec: {protocol: 'JSONSocket',
+                       transport: { protocol: 'UDP', port: 54321 },
+                      },
+            });
+            assert.exists(e = EndPoint.create(c.transport, {name: 'Test', spec: {address: '127.0.0.1'}})); 
+        });
+        it('object',       () => { assert.isObject(e); });
+        it('instanceOf',   () => { assert.instanceOf(e, EndPoint); });
+        it('has-name',     () => { assert.property(e, 'name'); });
+        it('has-spec',     () => { assert.property(e, 'spec'); });
+        it('has-protocol', () => { assert.propertyVal(e, 'protocol', 'UDP'); });
+    });
+});
