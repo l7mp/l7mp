07f9239aa9813cfe788f5bc9dc6c507f50751ef7
Váradi Richárd <ricsi19981007@gmail.com>
2020-07-08 09:16:40 +0200

    Remove bind and wrong stream cases from Websocket cluster

--- test/11-websocket-cluster.test.js
+++ test/11-websocket-cluster.test.js
@@ -20,10 +20,6 @@ describe('WebSocketCluster', () => {
         l7mp.run(); // should return
     });
 
-    // after( () => {
-    //     wss.close();
-    // })
-
     context('create', () => {
         c = Cluster.create({name: 'WebSocket', spec: {protocol: 'WebSocket'}});
         it('runs',             () => { assert.exists(c); });
@@ -42,29 +38,60 @@ describe('WebSocketCluster', () => {
 
     context('addEndPoint', () => {
         c = Cluster.create({name: 'WebSocket', spec: {protocol: 'WebSocket'}});
-        c.addEndPoint(EndPoint.create({protocol: 'WebSocket', name: 'WebSocket', spec: {port: 8080, bind: {address: '127.0.0.1', port: 8080}}},{spec: {address: '127.0.0.1', port: 8080}})); 
+        c.addEndPoint({name: 'Test', spec: {address: '127.0.0.1'}}); 
         var endpoint = c.endpoints[0];
         it('runs',             () => { assert.exists(c.endpoints); });
         it('object',           () => { assert.isObject(endpoint); });
         it('instanceOf',       () => { assert.instanceOf(endpoint, EndPoint); });
         it('has-name',         () => { assert.property(endpoint, 'name'); });
-        it('equal',            () => { assert.equal(endpoint.name, 'WebSocket-EndPoint-0'); });
+        it('equal',            () => { assert.equal(endpoint.name, 'Test'); });
         it('has-spec',         () => { assert.property(endpoint, 'spec'); });
         it('has-protocol',     () => { assert.deepPropertyVal(endpoint, 'protocol', 'WebSocket'); });
-        it('get',              () => { let n = c.getEndPoint('WebSocket-EndPoint-0'); assert.isOk(n); });
-        it('get-instanceOf',   () => { let n = c.getEndPoint('WebSocket-EndPoint-0'); assert.instanceOf(n, EndPoint); });
-        it('get-name',         () => { let n = c.getEndPoint('WebSocket-EndPoint-0'); assert.equal(n.name, 'WebSocket-EndPoint-0'); });
+        it('get',              () => { let n = c.getEndPoint('Test'); assert.isOk(n); });
+        it('get-instanceOf',   () => { let n = c.getEndPoint('Test'); assert.instanceOf(n, EndPoint); });
+        it('get-name',         () => { let n = c.getEndPoint('Test'); assert.equal(n.name, 'Test'); });
         it('get-fail',         () => { let n = c.getEndPoint('Never'); assert.isUndefined(n); });
-        it('delete',           () => { c.deleteEndPoint('WebSocket-EndPoint-0'); assert.lengthOf(c.endpoints, 0); });
-        it('get-fail',         () => { let n = c.getEndPoint('WebSocket'); assert.isUndefined(n); });
-        it('re-add',           () => { e = c.addEndPoint({name: 'WebSocket', spec: {}}); assert.isOk(e); });
-        it('get-2',            () => { let n = c.getEndPoint('WebSocket'); assert.isOk(n); });
-        it('get-2-name',       () => { let n = c.getEndPoint('WebSocket'); assert.equal(n.name, 'WebSocket'); });
+        it('delete',           () => { c.deleteEndPoint('Test'); assert.lengthOf(c.endpoints, 0); });
+        it('get-fail',         () => { let n = c.getEndPoint('Test'); assert.isUndefined(n); });
+        it('re-add',           () => { e = c.addEndPoint({name: 'Test', spec: {}}); assert.isOk(e); });
+        it('get-2',            () => { let n = c.getEndPoint('Test'); assert.isOk(n); });
+        it('get-2-name',       () => { let n = c.getEndPoint('Test'); assert.equal(n.name, 'Test'); });
+    });
+
+    context('addEndPoint-with-bind', () => {
+        c = Cluster.create({name: 'WebSocket', spec: {protocol: 'WebSocket'}});
+        c.addEndPoint({
+            name: 'Test', 
+            spec: { 
+                address: '127.0.0.1',
+                bind: {
+                    address: '127.0.0.1',
+                    port: 8080
+                }
+            }
+        });
+        var endpoint = c.endpoints[0];
+        it('runs',             () => { assert.exists(c.endpoints); });
+        it('object',           () => { assert.isObject(endpoint); });
+        it('instanceOf',       () => { assert.instanceOf(endpoint, EndPoint); });
+        it('has-name',         () => { assert.property(endpoint, 'name'); });
+        it('equal',            () => { assert.equal(endpoint.name, 'Test'); });
+        it('has-spec',         () => { assert.property(endpoint, 'spec'); });
+        it('has-protocol',     () => { assert.deepPropertyVal(endpoint, 'protocol', 'WebSocket'); });
+        it('get',              () => { let n = c.getEndPoint('Test'); assert.isOk(n); });
+        it('get-instanceOf',   () => { let n = c.getEndPoint('Test'); assert.instanceOf(n, EndPoint); });
+        it('get-name',         () => { let n = c.getEndPoint('Test'); assert.equal(n.name, 'Test'); });
+        it('get-fail',         () => { let n = c.getEndPoint('Never'); assert.isUndefined(n); });
+        it('delete',           () => { c.deleteEndPoint('Test'); assert.lengthOf(c.endpoints, 0); });
+        it('get-fail',         () => { let n = c.getEndPoint('Test'); assert.isUndefined(n); });
+        it('re-add',           () => { e = c.addEndPoint({name: 'Test', spec: {}}); assert.isOk(e); });
+        it('get-2',            () => { let n = c.getEndPoint('Test'); assert.isOk(n); });
+        it('get-2-name',       () => { let n = c.getEndPoint('Test'); assert.equal(n.name, 'Test'); });
     });
 
     context('stream', () => {
         var c = Cluster.create({name: 'WebSocket', spec: {protocol: 'WebSocket'}});
-        c.addEndPoint(EndPoint.create({protocol: 'WebSocket', name: 'WebSocket', spec: {port: 8080, bind: {address: '127.0.0.1', port: 8080}}},{spec: {address: '127.0.0.1', port: 8080}})); 
+        c.addEndPoint({name: 'Test', spec: {address: '127.0.0.1'}}); 
         var s;
         it('runs', async   () => { s = await c.stream({route: {retry: {timeout: 1000}}, metadata: {HTTP: {url: {host: '127.0.0.1', port: 8080}}}});});
         it('returns ok',   () => { assert.isOk(s.stream); });
@@ -93,35 +120,13 @@ describe('WebSocketCluster', () => {
             s = await c.stream({route:{retry:{timeout:100}}}).
                 catch(() => { assert.isOk(true);});
         });
-        it('fail-timeout-override', async () => {
-            e = c.endpoints[0]; e.mode=['ok']; e.timeout=1000;
-            let s = await c.stream({route:{retry:{timeout:100}}}).
-                catch(() => { assert.isOk(true);});
-        });
-        it('ok-fail-program', async () => {
-            e = c.endpoints[0]; e.mode=['ok', 'fail', 'ok', 'fail'];
-            e.timeout=0; e.round=0;
-            c.loadbalancer.update([e]); 
-            let i = 0;
-            let s1 = await c.stream({route:{retry:{timeout:2000}}, metadata: {HTTP: {url: {host: '127.0.0.1', port: 8080}}}}).then(
-                async () => {
-                    let s2 = await c.stream({route:{retry:{timeout:2000}}, metadata: {HTTP: {url: {host: '127.0.0.1', port: 8080}}}}).then(
-                        () => { assert.isOk(true); },
-                        async () => {
-                            let s3 = await c.stream({route:{retry:{timeout:2000}}, metadata: {HTTP: {url: {host: '127.0.0.1', port: 8080}}}}).then(
-                                async () => {
-                                    let s4 = await c.stream({route:{retry:{timeout:2000}}, metadata: {HTTP: {url: {host: '127.0.0.1', port: 8080}}}}).then(
-                                        () => { assert.fail(); },
-                                        () => { assert.isOk(true); }
-                                    );
-                                },
-                                () => { assert.fail(); }
-                            );
-                        }
-                    );
-                },
-                () => { assert.fail(); }
-            );
+        it('runs', async   () => { c.loadbalancer.update([c.endpoints[0]]); s = await c.stream({route: {retry: {timeout: 1000}}, metadata: {HTTP: {url: {host: '127.0.0.1', port: 8080}}}});});
+        it('close', (done) =>{
+            s.stream.on('finish', ()=>{
+                assert.isOk(true);
+                done();
+            });
+            s.stream.end();
         });
     });
 });
