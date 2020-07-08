1c203b9d01add389758a3870bf2aa12fbeb3ce73
Váradi Richárd <ricsi19981007@gmail.com>
2020-07-08 09:29:22 +0200

    Make changes based on #10

--- test/12-http-listener.test.js
+++ test/12-http-listener.test.js
@@ -9,24 +9,11 @@ const Session      = require('../session.js').Session;
 
 describe('HTTPListener', () => {
     var l, s, c;
-    var readers = {};
-    var reader = (name, stream, done) => {
-        let f = () => {
-            let data = ''; let chunk;
-            while (null !== (chunk = stream.read())) {
-                data += chunk;
-            }
-            assert.equal(data, 'test');
-            done();
-        };
-        readers[name] = f;
-        return f;
-    };
 
     before( () => {
         l7mp = new L7mp();
         l7mp.applyAdmin({ log_level: 'error' });
-        l7mp.run(); // should return
+        l7mp.run(); 
     });
 
     context('create', () => {
@@ -44,37 +31,13 @@ describe('HTTPListener', () => {
     context('#run', () => {
         it('runs', () => { 
             l.run(); 
-            var req = new Stream.Readable();
-            req.connection = { 
-                remoteAddress: '127.0.0.1', 
-                remotePort: 12345, 
-                localAddress: '127.0.0.1', 
-                localPort: 12345
-            };
-            req.httpVersion = 'HTTP/1.0';
-            req.method = 'GET';
-            req.url = '127.0.0.1';
-            req._read = function () {};
-            var res = new Stream.Writable();
-            l.server.emit('request', req, res); 
+            http.get('http:localhost:12345'); 
             assert.exists(l); 
         });
     });
 
     context('#connect', () => {
         it('connect',    (done) => { c = new Net.connect({port: 12345}, () => { assert.isOk(true); done(); }) });
-        // it('connect', (done) => { 
-        //     // const req = http.request({
-        //     //     port: 12345,
-        //     //     host: '127.0.0.1',
-        //     //     method: 'POST',
-        //     //     body: 'test'
-        //     // });
-        //     l.write('GET / HTTP/1.0\r\n' +
-        //          'Host: 127.0.0.1:12345\r\n' +
-        //          'Connection: close\r\n' +
-        //          '\r\n');
-        // console.log(s); done(); }); 
         it('address',        () => { assert.equal(c.localAddress, '127.0.0.1'); });
         it('remote-address', () => { assert.equal(c.remoteAddress, '127.0.0.1'); });
         it('remote-port',    () => { assert.equal(c.remotePort, 12345); });
@@ -96,7 +59,7 @@ describe('HTTPListener', () => {
         it('session-metadata-src-port',    () => { assert.nestedProperty(s, 'metadata.TCP.src_port'); });
         it('session-metadata-dst-port',    () => { assert.nestedPropertyVal(s, 'metadata.TCP.dst_port', 12345); });
         it('session-metadata-http',        () => { assert.nestedProperty(s, 'metadata.HTTP'); });
-        it('session-metadata-http-ver',    () => { assert.equal(s.metadata.HTTP.version, 'HTTP/1.0'); });
+        it('session-metadata-http-ver',    () => { assert.equal(s.metadata.HTTP.version, 1.1); });
         it('session-metadata-http-method', () => { assert.equal(s.metadata.HTTP.method, 'GET'); });
         it('session-metadata-http-url',    () => { assert.isObject(s.metadata.HTTP.url); });
         it('session-listener',             () => { assert.nestedPropertyVal(s, 'source.origin', 'HTTP'); });
@@ -104,45 +67,7 @@ describe('HTTPListener', () => {
         it('session-stream',               () => { assert.instanceOf(s.source.stream, Stream) });
         it('session-stream-readable',      () => { assert.isOk(s.source.stream.readable); });
         it('session-stream-writeable',     () => { assert.isOk(s.source.stream.writable); });
-        it('session-stream-priv-readable', () => { assert.instanceOf(s.priv.req, Stream.Readable); });
-        it('session-stream-priv-writable', () => { assert.instanceOf(s.priv.res, Stream.Writable); });
     });
-
-    // context('I/O', () => {
-    //     it('read', (done) => {
-    //         s.source.stream.on('readable', () => {
-    //             let data = ''; let chunk;
-    //             while (null !== (chunk = s.source.stream.read())) {
-    //                 data += chunk;
-    //             }
-    //             assert.equal(data, 'test');
-    //             done();
-    //         });
-
-    //         console.log(c.write(`
-    //             GET / HTTP/1.0
-    //             Host: 127.0.0.1:12345
-    //         `));     
-    //     });
-    //     it('write',  (done) => {
-    //         c.on('readable', () => {
-    //             let data = ''; let chunk;
-    //             while (null !== (chunk = c.read())) {
-    //                 data += chunk;
-    //             }
-    //             assert.equal(data, 'test');
-    //             done();
-    //         });
-    //         s.source.stream.write('test');
-    //     });
-    //     it('server-stream-end',  () => {
-    //         s.source.stream.removeAllListeners();
-    //         c.removeAllListeners();
-    //         s.source.stream.destroy();
-    //         assert.isOk(true);
-    //     });
-    //     it('client-stream-end',  () => { c.destroy(); assert.isOk(true); });
-    // });
 
     context('stop', () => {
         it('stop-server',  () => {
