357148178d816bf5f3fa4a8b15297c5a60929585
Váradi Richárd <ricsi19981007@gmail.com>
2020-07-08 11:14:33 +0200

    Add copyrights to tests

--- test/04-getAtPath-setAtPath.test.js
+++ test/04-getAtPath-setAtPath.test.js
@@ -19,6 +19,7 @@
 // AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 const util      = require('util');
 const assert    = require('chai').assert;
 const _         = require('lodash');
