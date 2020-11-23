f998482b417252d312d728fdf631d6b8cacc428e
Richard Varadi <ricsi19981007@gmail.com>
2020-11-23 12:02:46 +0100

    Put back the semicolons

--- test/14-loadbalancer.test.js
+++ test/14-loadbalancer.test.js
@@ -20,137 +20,136 @@
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-const assert       = require('chai').assert
-const L7mp         = require('../l7mp.js').L7mp
-const LoadBalancer = require('../cluster.js').LoadBalancer
-const EndPoint     = require('../cluster.js').EndPoint
+const assert       = require('chai').assert;
+const L7mp         = require('../l7mp.js').L7mp;
+const LoadBalancer = require('../cluster.js').LoadBalancer;
+const EndPoint     = require('../cluster.js').EndPoint;
 const HashRing      = require('hashring');
 const { NotFoundError } = require('../error.js');
 
 describe('LoadBalancing', () => {
     before( () => {
-        l7mp = new L7mp()
+        l7mp = new L7mp();
         // l7mp.applyAdmin({ log_level: 'silly' })
-        l7mp.applyAdmin({ log_level: 'error' })
-        l7mp.run() 
-    })
+        l7mp.applyAdmin({ log_level: 'error' });
+        l7mp.run() ;
+    });
 
     context('Base class', () => {
-        let lb 
+        let lb;
         it('Create', () => {
-            lb = LoadBalancer.create({policy: 'None'})
-            assert.exists(lb)
-        })
+            lb = LoadBalancer.create({policy: 'None'});
+            assert.exists(lb);
+        });
         it('Update', () => {
-            lb.update([{test: 'test'}])
-            assert.propertyVal(lb.es[0], 'test', 'test')
-        })
+            lb.update([{test: 'test'}]);
+            assert.propertyVal(lb.es[0], 'test', 'test');
+        });
         it('Apply', () => {
-            assert.isNotOk(lb.apply([]))
-        })
+            assert.isNotOk(lb.apply([]));
+        });
         it('toJSON', () => {
-            let lbJSON = lb.toJSON()
-            assert.propertyVal(lbJSON, 'policy', 'None')
-        })
-    })
+            let lbJSON = lb.toJSON();
+            assert.propertyVal(lbJSON, 'policy', 'None');
+        });
+    });
 
     context('Default', () => {
         it('Non-exist policy', () => {
-            assert.throws(() => LoadBalancer.create({policy: 'none'}), Error, 'LoadBalancer.create: TODO: Policy "none" unimplemented')
-        })
+            assert.throws(() => LoadBalancer.create({policy: 'none'}), Error, 'LoadBalancer.create: TODO: Policy "none" unimplemented');
+        });
 
         it('Empty object', () => {
-            assert.throws(() => LoadBalancer.create({}), Error, 'LoadBalancer.create: TODO: Policy undefined')
-        })
-    })
+            assert.throws(() => LoadBalancer.create({}), Error, 'LoadBalancer.create: TODO: Policy undefined');
+        });
+    });
 
     context('Trivial', () => {
-        var tl, es 
+        var tl, es;
         it('create', () => { 
-            tl = LoadBalancer.create({policy: 'Trivial'}) 
-            assert.exists(tl) })
+            tl = LoadBalancer.create({policy: 'Trivial'});
+            assert.exists(tl);
+        });
         it('no-endpoint', () => {
             try {
-                tl.apply({})
+                tl.apply({});
             } catch (error) {
-                assert.isOk(true)
+                assert.isOk(true);
             }
-        })
+        });
         it('single-es', () => {
-            tl.update([EndPoint.create({protocol: 'Test'}, {name: 'Test', spec: {}})])
-            assert.isNotEmpty(tl.es)
-        })
-        it('es-length', () => { assert.lengthOf(tl.es, 1) })
+            tl.update([EndPoint.create({protocol: 'Test'}, {name: 'Test', spec: {}})]);
+            assert.isNotEmpty(tl.es);
+        });
+        it('es-length', () => { assert.lengthOf(tl.es, 1); });
         it('single-es-apply', () => { 
-            es = tl.apply({}) 
-            assert.exists(es) 
-        }) 
-        it('es-class', () => { assert.instanceOf(es, EndPoint) })
+            es = tl.apply({});
+            assert.exists(es);
+        });
+        it('es-class', () => { assert.instanceOf(es, EndPoint); });
         it('multiple-endpoints', () => {
             tl.update([
                 EndPoint.create({protocol: 'Test'}, {name: 'Test1', spec: {}}), 
                 EndPoint.create({protocol: 'Test'}, {name: 'Test2', spec: {}}),
                 EndPoint.create({protocol: 'Test'}, {name: 'Test3', spec: {}})
-            ])
-            assert.lengthOf(tl.es, 3)
-        })
+            ]);
+            assert.lengthOf(tl.es, 3);
+        });
         it('multiple-apply', () => {
-            es = tl.apply({})
-            assert.exists(es)
-        })
-        it('first', () => { assert.equal(tl.es[0], es) })
-    })
+            es = tl.apply({});
+            assert.exists(es);
+        });
+        it('first', () => { assert.equal(tl.es[0], es); });
+    });
 
     context('Hash', () => {
-        var hl, hlkey
+        var hl, hlkey;
         it('create-without-key', () => { 
-            hl = LoadBalancer.create({policy: 'HashRing'}) 
-            assert.exists(hl) 
-        })
+            hl = LoadBalancer.create({policy: 'HashRing'});
+            assert.exists(hl);
+        });
         it('create-with-key', () => {
-            hlkey = LoadBalancer.create({policy: 'HashRing', key: '/key'}) 
-            assert.exists(hlkey) 
-        })
+            hlkey = LoadBalancer.create({policy: 'HashRing', key: '/key'});
+            assert.exists(hlkey);
+        });
         it('toJSON()', () => {
-            let hlkeyJSON = hlkey.toJSON()
-            assert.propertyVal(hlkeyJSON, 'policy', 'HashRing')
-            assert.propertyVal(hlkeyJSON, 'key', '/key')
-        })
+            let hlkeyJSON = hlkey.toJSON();
+            assert.propertyVal(hlkeyJSON, 'policy', 'HashRing');
+            assert.propertyVal(hlkeyJSON, 'key', '/key');
+        });
 
         context('Update', () =>{
             let endpoints = [
                 { endpoint: 'E1', name: 'E1', weight: 1, spec: {address: '0.0.0.0'}},
                 { endpoint: 'E2', name: 'E2', weight: 2, spec: {address: '0.0.0.1'}}
-            ]
+            ];
             it('update()', () => {
-                hlkey.update(endpoints)
-                assert.hasAllKeys(hlkey.keys, ['0.0.0.0', '0.0.0.1'])
-            })
+                hlkey.update(endpoints);
+                assert.hasAllKeys(hlkey.keys, ['0.0.0.0', '0.0.0.1']);
+            });
             it('hashring', () => {
-                assert.instanceOf(hlkey.hashring, HashRing)
-            })
-        })
+                assert.instanceOf(hlkey.hashring, HashRing);
+            });
+        });
 
         context('Apply', () => {
             it('No-keys', () => {
-                assert.throws(() => hl.apply({name: 'Test'}), NotFoundError)
-            })
+                assert.throws(() => hl.apply({name: 'Test'}), NotFoundError);
+            });
             it('No-key-found', () => {
-                assert.isOk(hlkey.apply({name: 'Test', metadata: {keys: '1.1.1.1'}}))
-            })
+                assert.isOk(hlkey.apply({name: 'Test', metadata: {keys: '1.1.1.1'}}));
+            });
             it('Matching-key', () => {
-                assert.propertyVal(hlkey.apply({name: 'Test', metadata: {key: '0.0.0.1'}}), 'name', 'E2')
-            })
-        })
-        
-    })
+                assert.propertyVal(hlkey.apply({name: 'Test', metadata: {key: '0.0.0.1'}}), 'name', 'E2');
+            });
+        });
+    });
 
     context('ConsistentHash', () => {
-        var chl
+        var chl;
         it('Create', () => {
-            chl = LoadBalancer.create({policy: 'ConsistentHash'})
-            assert.exists(chl)
-        })
-
-    })
-})
+            chl = LoadBalancer.create({policy: 'ConsistentHash'});
+            assert.exists(chl);
+        });
+    });
+});
