9d9c59d4beb037f4fd56351e51b9357ef0ae55b8
Tamas Levai <levait@tmit.bme.hu>
2021-02-06 16:36:49 +0100

    Add rudimentary UDP kernel offload implementation
    FIXME:
    - globally pinned bpf maps
    - statistics metrics are not handled
    - bpf object is not compiled automatically if object file does not
      exist
    - no L7MP integration; just a standalone demo
    
    Demo code:
    
    ```js
    "use strict";
    
    const uko = require("./udp-kernel-offload.js");
    
    function Flow(src_ip4=0, src_port=0, dst_ip4=0, dst_port=0, proto=0) {
        this.src_ip4 = src_ip4;
        this.src_port = src_port;
        this.dst_ip4 = dst_ip4;
        this.dst_port = dst_port;
        this.proto = proto;
    }
    
    function convertFlowToBuffer(flow, map, buf_size=16) {
        var buf = Buffer.alloc(buf_size);
        buf.writeUInt32BE(flow.src_ip4, 0);
        buf.writeUInt32BE(flow.dst_ip4, 4);
        buf.writeUInt16BE(flow.src_port, 8);
        buf.writeUInt16BE(flow.dst_port, 10);
        buf.writeUInt32LE(flow.proto, 12);
        return buf;
    }
    
    ///////////////////////////////
    
    var inFlow = new Flow(0, 0, 0xa000001, 1234, 17);
    var redirFlow = new Flow(0xa000001, 1235, 0x7f000001, 1235, 17);
    
    uko.initOffloadEngine();
    uko.requestOffload(inFlow, redirFlow, "create");
    const stats = uko.getStat(inFlow);
    console.log(stats);  // expect to see FlowStat(0, 0, 0)
    uko.shutdownOffloadEngine();
    ```

--- udp-kernel-offload.js
+++ udp-kernel-offload.js
@@ -1,21 +1,38 @@
-const bpf = require('bpf');
+"use strict";
+
+const child_process = require("child_process");
+const fs = require("fs");
+const os = require("os");
+
+const bpf = require("bpf");
 
 
-function flow(src_ip4=0, src_port=0, dst_ip4=0, dst_port=0, proto=0) {
-    this.src_ip4 = src_ip4;   // uint32_t src_ip4;
-    this.src_port = src_port; // uint16_t src_port;
-    this.dst_ip4 = dst_ip4;   // uint32_t dst_ip4;
-    this.dst_port = dst_port; // uint16_t dst_port;
-    this.proto = proto;       // uint32_t proto;
+const REDIRMAP_PATH = "/sys/fs/bpf/tc/globals/sidecar_redirects";
+const STATMAP_PATH = "/sys/fs/bpf/tc/globals/sidecar_statistics";
+
+const BPF_OBJ_FILE = "kernel-offload/l7mp-uko.o";
+
+var redirMap;
+var statisticsMap;
+
+var statistics = {};
+
+
+function Flow(src_ip4=0, src_port=0, dst_ip4=0, dst_port=0, proto=0) {
+    this.src_ip4 = src_ip4;
+    this.src_port = src_port;
+    this.dst_ip4 = dst_ip4;
+    this.dst_port = dst_port;
+    this.proto = proto;
 }
 
-function flow_stat(pkts=0, bytes=0, timestamp_last=0) {
-    this.pkts = BigInt(pkts);                     // uint64_t pkts;
-    this.bytes = BigInt(bytes);	                  // uint64_t bytes;
-    this.timestamp_last = BigInt(timestamp_last); // uint64_t timestamp_last;
+function FlowStat(pkts=0, bytes=0, timestamp_last=0) {
+    this.pkts = BigInt(pkts);
+    this.bytes = BigInt(bytes);
+    this.timestamp_last = BigInt(timestamp_last);
 }
 
-function convert_flow_to_buffer(flow, map, buf_size=16) {
+function convertFlowToBuffer(flow, map, buf_size=16) {
     var buf = Buffer.alloc(buf_size);
     buf.writeUInt32BE(flow.src_ip4, 0);
     buf.writeUInt32BE(flow.dst_ip4, 4);
@@ -25,18 +42,18 @@ function convert_flow_to_buffer(flow, map, buf_size=16) {
     return buf;
 }
 
-function convert_buffer_to_flow(buf) {
-    var flow_ = new flow(
-	src_ip4 = buf.readUInt32BE(0),
-	dst_ip4 = buf.readUInt32BE(4),
-	src_port = buf.readUInt16BE(8),
-	dst_port = buf.readUInt16BE(10),
-	proto = buf.readUInt32LE(12),
+function convertBufferToFlow(buf) {
+    var flow_ = new Flow(
+        buf.readUInt32BE(0),  // src_ip4
+        buf.readUInt32BE(4),  // dst_ip5
+        buf.readUInt16BE(8),  // src_port
+        buf.readUInt16BE(10), // dst_port
+        buf.readUInt32LE(12)  // proto
     );
     return flow_;
 }
 
-function convert_flow_stat_to_buffer(flow_stat, buf_size=24) {
+function convertFlowStatToBuffer(flow_stat, buf_size=24) {
     var buf = Buffer.alloc(buf_size);
     buf.writeBigUInt64LE(BigInt(flow_stat.pkts), 0);
     buf.writeBigUInt64LE(BigInt(flow_stat.bytes), 8);
@@ -44,73 +61,145 @@ function convert_flow_stat_to_buffer(flow_stat, buf_size=24) {
     return buf;
 }
 
-function convert_buffer_to_flow_stat(buf) {
-    var flow_stat_ = new flow_stat(
-	pkts = buf.readBigUInt64LE(0),
-	bytes = buf.readBigUInt64LE(8),
-	timestamp_last = buf.readBigUInt64LE(16),
+function convertBufferToFlowStat(buf) {
+    var flow_stat_ = new FlowStat(
+        buf.readBigUInt64LE(0),  // pkts
+        buf.readBigUInt64LE(8),  // bytes
+        buf.readBigUInt64LE(16)  // timestamp_last
     );
     return flow_stat_;
 }
 
 
-///////////////////////////////////
 
+function loadBpf(ifName, bpfObjFile="sidecar-kern.o") {
+    // Load bpf object with 'tc'
 
-// open maps
-const REDIRMAP_PATH = "/sys/fs/bpf/tc/globals/sidecar_redirects"
-const STATMAP_PATH = "/sys/fs/bpf/tc/globals/sidecar_statistics"
+    // Check if clsact is available..
+    const qdiscInfoCmdArgs = ["qdisc", "list", "dev", ifName];
+    const qdiscInfoCmd = child_process.spawnSync("tc", qdiscInfoCmdArgs,
+                                                 { encoding: "utf-8" });
+    const clsactIsLoaded = qdiscInfoCmd.stdout.includes("clsact");
 
-const redir_map_fd = bpf.objGet(REDIRMAP_PATH);
-const stat_map_fd = bpf.objGet(STATMAP_PATH);
+    // ..Add clsact if not
+    if (clsactIsLoaded === false) {
+        const qdiscAddCmdArgs = ["qdisc", "add", "dev", ifName, "clsact"];
+        const qdiscAddCmd = child_process.spawnSync("tc", qdiscAddCmdArgs,
+                                                    { encoding: "utf-8" });
+        if (qdiscAddCmd.status !== 0) {
+            throw new Error("Error during qdisc creation\n" +
+                            "retval: " + qdiscAddCmd.status +
+                            "\noutput:" + qdiscAddCmd.output);
+        }
+    }
 
-const redir_map = new bpf.RawMap(bpf.createMapRef(redir_map_fd, { transfer: true }));
-const stat_map = new bpf.RawMap(bpf.createMapRef(stat_map_fd, { transfer: true }));
-
-// write to maps
-var flow_in = new flow(
-    src_ip4 = 0,
-    src_port = 0,
-    dst_ip4 = 0xa000001,
-    dst_port = 1234,
-    proto = 17,
-);
-var flow_redir = new flow(
-    src_ip4 = 0xa000001,
-    src_port = 1235,
-    dst_ip4 = 0x7f000001,
-    dst_port = 1235,
-    proto = 17,
-);
-redir_map.set(convert_flow_to_buffer(flow_in), convert_flow_to_buffer(flow_redir));
-
-var flow_in2 = new flow(
-    src_ip4 = 0,
-    src_port = 0,
-    dst_ip4 = 0x7f000001,
-    dst_port = 1237,
-    proto = 17,
-);
-var flow_redir2 = new flow(
-    src_ip4 = 0xa000001,
-    src_port = 1236,
-    dst_ip4 = 0xa000002,
-    dst_port = 1236,
-    proto = 17,
-);
-redir_map.set(convert_flow_to_buffer(flow_in2), convert_flow_to_buffer(flow_redir2));
-
-
-// read from maps
-var stat_buf = stat_map.getPerCPU(convert_flow_to_buffer(flow_in));
-
-var flow_stat_agg = new flow_stat();
-for (var i = 0; i < stat_buf.length; i+=stat_map.ref.valueSize) {
-    var stats = convert_buffer_to_flow_stat(stat_buf.slice(i, i+stat_map.ref.valueSize));
-    flow_stat_agg.pkts += stats.pkts;
-    flow_stat_agg.bytes += stats.bytes;
-    if (stats.timestamp_last > flow_stat_agg.timestamp_last)
-	flow_stat_agg.timestamp_last = stats.timestamp_last;
+    // Load BPF code to interface
+    const command = (
+        clsactIsLoaded ? "replace" : "add"
+    );
+    const tcArgs = ["filter", command, "dev", ifName, "ingress", "bpf",
+                    "direct-action", "object-file", bpfObjFile,
+                    "section", "classifier"];
+    const tcLoadBpfCmd = child_process.spawnSync("tc", tcArgs,
+                                                 { encoding: "utf-8" });
+    if (tcLoadBpfCmd.status !== 0) {
+        throw new Error("Error during BPF code loading\n" +
+                        "retval: " + tcLoadBpfCmd.status +
+                        "\noutput: " + tcLoadBpfCmd.output);
+    }
 }
 
-console.log(flow_stat_agg);
+function unloadBpf(ifName) {
+    // Unload BPF code with 'tc'
+    const tcArgs = ["filter", "del", "dev", ifName, "ingress"];
+    const tcLoadBpfCmd = child_process.spawnSync("tc", tcArgs,
+                                                 { encoding: "utf-8" });
+    if (tcLoadBpfCmd.status !== 0) {
+        throw new Error("Error during BPF code unloading\n" +
+                        "retval: " + tcLoadBpfCmd.status +
+                        "\noutput: " + tcLoadBpfCmd.output);
+    }
+}
+
+
+function initOffloadEngine() {
+    //  Create maps and objs
+
+    // const redirectsMap = bpf.createMap({  // TODO
+    //     type: bpf.MapType.LRU_HASH,
+    //     name: "sidecar_redirects",
+    //     keySize: 16,
+    //     valueSize: 16,
+    //     maxEntries: 10240
+    // });
+
+    // const statisticsMap = bpf.createMap({
+    //     type: bpf.MapType.LRU_HASH,
+    //     name: "sidecar_statistics",
+    //     keySize: 16,
+    //     valueSize: 24,
+    //     maxEntries: 10240
+    // });
+
+    // console.log(redirectsMap, "\nfd: ", redirectsMap.fd, "\n");
+    // console.log(statisticsMap, "\nfd: ", statisticsMap.fd, "\n");
+
+
+    //  Load BPF object on all interfaces
+    for (const ifName of Object.keys(os.networkInterfaces())) {
+        loadBpf(ifName, BPF_OBJ_FILE);
+    }
+
+    // Connect maps
+    global.redirectsMap = new bpf.RawMap(
+        bpf.createMapRef(bpf.objGet(REDIRMAP_PATH), { transfer: true })
+    );
+    global.statisticsMap = new bpf.RawMap(
+        bpf.createMapRef(bpf.objGet(STATMAP_PATH), { transfer: true })
+    );
+}
+
+function shutdownOffloadEngine() {
+   for (const ifName of Object.keys(os.networkInterfaces())) {
+        unloadBpf(ifName);
+    }
+    fs.unlinkSync(REDIRMAP_PATH);
+    fs.unlinkSync(STATMAP_PATH);
+}
+
+function requestOffload(inFlow, redirFlow, action, metrics=null) {
+    //  Action param: “create”, “remove”
+    switch (action) {
+    case "create":
+        //  Register 5-tuple to statistics and redirects map
+        const zeroStatBuf = Buffer.alloc(global.statisticsMap.ref.valueSize, 0);
+        global.statisticsMap.set(convertFlowToBuffer(inFlow), zeroStatBuf);
+        global.redirectsMap.set(convertFlowToBuffer(inFlow),
+                                convertFlowToBuffer(redirFlow));
+        break;
+    case "remove":
+        // Delete 5-tuple from both redirects and statistics maps
+        global.redirectsMap.delete(convertFlowToBuffer(inFlow));
+        global.statisticsMap.delete(convertFlowToBuffer(inFlow));
+        break;
+    default:
+        throw new Error("Invalid action for requestOffload");
+    }
+
+    //  Store requested metrics
+    // TODO
+}
+
+function getStat(inFlow) {
+    //  Collect metrics:
+    const statBuf = global.statisticsMap.get(convertFlowToBuffer(inFlow));
+    statistics[inFlow] = convertBufferToFlowStat(statBuf);
+    return statistics[inFlow];
+}
+
+module.exports = {
+    initOffloadEngine,
+    shutdownOffloadEngine,
+    requestOffload,
+    getStat
+};
