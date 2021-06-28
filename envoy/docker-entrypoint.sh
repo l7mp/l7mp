abbddc85efcb6142a19b1ed251958262560ae880
Kornel David <konel1020@gmail.com>
2021-06-28 14:22:54 +0200

    Fix envoy config generator and adding necessary k8s resources

--- envoy/docker-entrypoint.sh
+++ envoy/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Generating envoy.yaml config file..."
-cat /tmpl/envoy.yaml.tmpl | envsubst \$NODE_POD_UID,\$GRPC_SERVICE_NAME > /etc/envoy.yaml
+cat /tmpl/envoy.yaml.tmpl | envsubst \$NODE_POD_UID,\$GRPC_SERVICE_NAME,\$GRPC_SERVICE_PORT,\$NG_CONTROL_LISTENER_PORT,\$NG_CONTROL_CLUSTER_ENDPOINT_ADDRESS,\$NG_CONTROL_CLUSTER_ENDPOINT_PORT > /etc/envoy.yaml
 
 echo "Starting Envoy..."
-/usr/local/bin/envoy -c /etc/envoy.yaml
+/usr/local/bin/envoy -c /etc/envoy.yaml -l info --drain-time-s 1
